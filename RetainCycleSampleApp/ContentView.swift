25826206053e81de9b03a1467fc01270fe62c67f
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-02-12 22:45:47 +0100

    Base project

---
+++ RetainCycleSampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RetainCycleSampleApp
+//
+//  Created by Javier Calatrava on 12/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RetainCycleSampleApp/RetainCycleSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetainCycleSampleAppApp.swift
+//  RetainCycleSampleApp
+//
+//  Created by Javier Calatrava on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct RetainCycleSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
